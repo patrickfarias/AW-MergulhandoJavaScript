532d47b9bf2d3c9399e265c9bb4c4353b6f62a01
“patrickfarias” <patrickfarias@icloud.com>
2021-03-12 23:14:13 -0300

    Aula 2.5 - O prinicipio de funcoes

--- mjs/script.js
+++ mjs/script.js
@@ -1,34 +1,19 @@
-var person1 = {
+// criando funcoes
 
-    name: 'Patrick',
-    age: 22,
-    sex: 'male'
+// funcao sem retorno
+function greet(name = 'Mergulhador'){
+    console.log('Boa Madrugada, ' + name)
 }
 
-var person2 = {
+greet('Jose')
+greet()
 
-    name: 'Jose',
-    age: 22,
-    sex: 'male'
+
+// funcao com retorno
+function greetComRetorno(name = 'Mergulhador'){
+    return 'Boa Madrugada, ' + name
 }
 
-person1.age = 30
-
-console.log(person1)
-console.log(person1.age)
-
-var list = [ person1, person2 ]
-
-console.log(list[0].sex)
-
-// Estrutura de laco
-for (var person of list ){
-
-    console.log(person)
-}
-
-
-
-
+console.log(greetComRetorno('Daniel'))
 
 
